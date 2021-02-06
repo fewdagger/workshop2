f96aed706c3feed8193d55ca80cb57cea974394a
fewdagger <sakit.t@ku.th>
2021-02-06 21:41:58 +0700

    add new excercise(while,for)

---
+++ while_for_exercise1.py
@@ -0,0 +1,22 @@
+# while
+i = 2
+while i <= 12:
+    j = 1
+    print("  [", i, "]")
+    while j <= 12:
+        print(i, "*", j, ":", i * j, end="\n")
+        j += 1
+    print("---------------")
+    i += 1
+
+
+# for
+i = 2
+for i in range(2, 12):
+    j = 1
+    print("  [", i, "]")
+    for j in range(2, 12):
+        print(i, "*", j, ":", i * j, end="\n")
+        j += 1
+    print("---------------")
+    i += 1
