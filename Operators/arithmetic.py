5036e8c27a47177a688af801835d384b41cbd0cc
fewdagger <sakit.t@ku.th>
2021-02-06 12:07:43 +0700

    add new folder(operator,range,for,while,if,dict,)

---
+++ Operators/arithmetic.py
@@ -0,0 +1,10 @@
+x = 17
+y = 12
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
---
+++ Operators/comparison.py
@@ -0,0 +1,9 @@
+x = 17
+y = 2
+
+print("x > y is", x > y)
+print("x < y is", x < y)
+print("x == y is", x == y)
+print("x != y is", x != y)
+print("x >= y is", x >= y)
+print("x <= y is", x <= y)
