e5329eebc12d1679f2680c67949993f4c250c8f5
fewdagger <sakit.t@ku.th>
2021-02-06 22:25:33 +0700

    no message

--- while_for_exercise1.py
+++ while_for_exercise1.py
@@ -12,10 +12,10 @@ while i <= 12:
 
 # for
 i = 2
-for i in range(2, 12):
+for i in range(2, 13):
     j = 1
     print("  [", i, "]")
-    for j in range(2, 12):
+    for j in range(2, 13):
         print(i, "*", j, ":", i * j, end="\n")
         j += 1
     print("---------------")
